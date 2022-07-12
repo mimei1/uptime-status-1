4dd7f564f5af0f4240690df3f9b5ee86d124bbc7
迷妹 <101113551+mimei1@users.noreply.github.com>
2022-07-12 15:57:57 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -5,21 +5,18 @@ window.Config = {
   SiteName: 'Uptime Status',
 
   // 站点链接
-  SiteUrl: '/',
+  SiteUrl: 'status.27sex.com',
 
   // UptimeRobot Api 域名
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'status.27sex.com',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1793209-b74bf9a89218101a17d5f501',
   ],
 
   // 是否显示监测站点的链接
